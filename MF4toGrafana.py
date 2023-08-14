979b307809cc8030e130a12aaf8de63a25cb63e1
Ondrej Luks <ondrej.luks@doosan.com>
2023-08-14 09:25:51 +0200

    Database upload works, aggregation doesnt

--- MF4toGrafana.py
+++ MF4toGrafana.py
@@ -1,8 +1,88 @@
 from asammdf import MDF
-import cantools as ct
+from sqlalchemy import create_engine, schema
+from sqlalchemy.sql import text
 import pandas as pd
 import os
 import json
+
+# ===========================================================================================================
+# ===========================================================================================================
+
+class DatabaseHandle:
+    def __init__(self, config):
+        self.schema_name = config["database"]["schema_name"]
+        self.host = config["database"]["host"]
+        self.port = config["database"]["port"]
+        self.database = config["database"]["database"]
+        self.user = config["database"]["user"]
+        self.password = config["database"]["password"]
+
+        self.conn_string = "postgresql://" + self.user + ":" + self.password + "@" + self.host + "/" + self.database
+
+
+    def connect(self) -> bool:
+        try:
+            self.engine = create_engine(self.conn_string)
+            self.connection = self.engine.connect()
+
+        except Exception as e:
+            print()
+            print(f"ERROR: {e}")
+            return False
+        
+        return True
+    
+
+    def create_schema(self) -> bool:
+        try:
+            if self.connection.dialect.has_schema(self.connection, self.schema_name):
+                print(f"Dropping schema {self.schema_name}")
+                drop_schema_sql = text(f"DROP SCHEMA {self.schema_name} CASCADE")
+                self.connection.execute(drop_schema_sql)
+                self.connection.commit()
+                
+            print(f"Creating schema {self.schema_name}")
+            self.connection.execute(schema.CreateSchema(self.schema_name))
+            self.connection.commit()
+
+        except Exception as e:
+            print()
+            print(f"ERROR: {e}")
+            return False
+        
+        return True
+    
+
+    def upload_data(self, data: list) -> bool:
+        try:
+            for idx, df in enumerate(data):
+                table_name = f"{df.columns.values[0]}"
+                df.to_sql(name=table_name,
+                          con=self.engine,
+                          schema=self.schema_name,
+                          index=True,
+                          index_label="time_stamp",
+                          if_exists="append")
+                self.connection.commit()
+
+        except Exception as e:
+            print()
+            print(f"ERROR: {e}")
+            return False
+        
+        return True
+    
+
+    def finish(self) -> bool:
+        try:
+            # self.cursor.close()
+            self.connection.close()
+        except Exception as e:
+            print()
+            print(f"ERROR: {e}")
+            return False
+        
+        return True
 
 # ===========================================================================================================
 # ===========================================================================================================
@@ -14,6 +94,7 @@ def open_config():
         data = json.load(file)
         file.close()
     except FileNotFoundError:
+        print()
         print("ERROR while reading config.json file. Check for file existance.")
         return
     
@@ -28,9 +109,10 @@ def create_dbc_set() -> set:
     try:
         for dbc_file in os.listdir("DBCfiles"):
             # TODO - WHAT ABOUT BUS?
-            converter_db["CAN"].append((dbc_file, 1))
+            converter_db["CAN"].append((os.path.join("DBCfiles", dbc_file), 1))
 
     except OSError:
+        print()
         print("ERROR while loading DBC files. Check for folder existance.")
         return
 
@@ -44,15 +126,26 @@ def convert_mf4(mf4_file: os.path, dbc_set: set) -> pd.DataFrame:
     # extract CAN messages
     extracted_mdf = mdf.extract_bus_logging(dbc_set)
     # return MDF converted to dataframe
-    return extracted_mdf.to_dataframe()
+    mdf_df = extracted_mdf.to_dataframe(time_from_zero=False, time_as_date=True)
+    mdf_df.index = pd.to_datetime(mdf_df.index)
+    return mdf_df
 
 
 def aggregate(df_set) -> set:
+    """Aggregates input set of dataframes by removing redundant values"""
+    result_list = []
     for df in df_set:
-        # TODO AGGREGATION 
-        continue
-    
-    return df_set
+        if df.shape[0] > 0:
+            # remove repeating values, leave only 1st and last in repeating series
+            mask = ((df[df.columns.values[0]] != df[df.columns.values[0]].shift()) |(df[df.columns.values[0]] != df[df.columns.values[0]].shift(-1)))
+            # leave data every 30 minutes, if previous operation deletes too much
+            # time_diffs = df.index.to_series().diff()
+            # mask |= time_diffs >= pd.Timedelta(minutes=30)
+            # apply mask
+            result_df = df[mask]
+            result_list.append(result_df)
+        
+    return result_list
 
 
 def split_df_by_cols(df) -> set:
@@ -63,7 +156,16 @@ def split_df_by_cols(df) -> set:
 
 
 def process_handle(dbc_set: set, config) -> bool:
+    """Function that handles MF4 files process from conversion to upload"""
     try:
+        # prepare the database
+        db = DatabaseHandle(config)
+    
+        if not db.connect():
+            return False
+        if not db.create_schema():
+            return False
+        
         # search for MF4 files
         for root, dirs, files in os.walk("SourceMF4"):
             # mostly "files" consists of only 1 file
@@ -83,17 +185,22 @@ def process_handle(dbc_set: set, config) -> bool:
                     # aggregate if neccessary
                     if config["settings"]["aggregate"]:
                         print("aggregating... ", end="")
-                        mf4_df = aggregate(column_dataframes)
-
+                        column_dataframes = aggregate(column_dataframes)
+                    
                     # upload to the db
                     print("uploading... ", end="")
-                    # TODO
+                    if not db.upload_data(column_dataframes):
+                        return False
 
                     print(" done!")
                     print()
 
+        # close database connection
+        if not db.finish():
+            return False
+
     except OSError:
-        print("ERROR while loading MF4 files. Check for folder existance.")
+        print("ERROR while loading MF4 files. Check for DBC and MF4 existance.")
         return False
     
     return True
--- install/installModules.bat
+++ install/installModules.bat
@@ -1,8 +1,13 @@
 @echo off
 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+python -m pip install --upgrade pip
+
 pip install numpy
 pip install pandas
-pip install cantools
+pip install psycopg2
+pip install sqlalchemy
 pip install asammdf
 
 echo:
