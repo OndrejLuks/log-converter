69692fe0c3bda92638f633844105ac38467004bf
Ondrej Luks <ondrej.luks@doosan.com>
2023-10-02 13:26:01 +0200

    GUI - Added text input and text view into the simple test program

--- gui.py
+++ gui.py
@@ -1,14 +1,19 @@
 import customtkinter
 
 class TrueFalseFrame(customtkinter.CTkFrame):
-    def __init__(self, master, values):
+    def __init__(self, master, title, values):
         super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
         self.values = values
+        self.title = title
         self.switches = []
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="orange", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
 
         for i, value in enumerate(self.values):
             switch = customtkinter.CTkSwitch(self, text=value)
-            switch.grid(row=i, column=0, padx=10, pady=5, sticky="w")
+            switch.grid(row=i+1, column=0, padx=10, pady=5, sticky="w")
             self.switches.append(switch)
 
     def get(self):
@@ -23,19 +28,27 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("Simple test")
-        self.geometry("400x200")
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
 
-        self.checkbox_frame = TrueFalseFrame(self, values=["Option 1", "Option 2", "Option 3", "Option 4"])
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        self.checkbox_frame = TrueFalseFrame(self, title="Select T/F:", values=["Option 1", "Option 2", "Option 3", "Option 4"])
         self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nswe")
+        
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="Tu zadaj text...")
+        self.entry.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+
+        self.textbox = customtkinter.CTkTextbox(master=self)
+        self.textbox.grid(row=1, column=0, padx=10, pady=10, sticky="nsew", columnspan=2)
 
         self.btn = customtkinter.CTkButton(self, text="Click me!", command=self.btn_callback)
-        self.btn.grid(row=1, column=0, padx=10, pady=10, columnspan=2, sticky="ew")
+        self.btn.grid(row=2, column=0, padx=10, pady=10, columnspan=2, sticky="ew")
 
 
     def btn_callback(self):
+        self.textbox.insert("end", f"You just inserted:\n{self.entry.get()}\n")
+        self.textbox.see("end")
         print("Oi, selected: ", self.checkbox_frame.get())
 
 
