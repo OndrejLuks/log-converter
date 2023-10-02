9948c100b34fafb1ae3db2b5dd7a40225a4f048c
Ondrej Luks <ondrej.luks@doosan.com>
2023-10-02 12:25:07 +0200

    GUI - simple window improved with dyamic number of items

--- gui.py
+++ gui.py
@@ -1,25 +1,20 @@
 import customtkinter
 
-class CheckboxFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
+class TrueFalseFrame(customtkinter.CTkFrame):
+    def __init__(self, master, values):
         super().__init__(master)
+        self.values = values
+        self.switches = []
 
-        self.checkbox_1 = customtkinter.CTkSwitch(self, text="Option 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-        self.checkbox_2 = customtkinter.CTkSwitch(self, text="Option 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-        self.checkbox_3 = customtkinter.CTkSwitch(self, text="Option 3")
-        self.checkbox_3.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-
+        for i, value in enumerate(self.values):
+            switch = customtkinter.CTkSwitch(self, text=value)
+            switch.grid(row=i, column=0, padx=10, pady=5, sticky="w")
+            self.switches.append(switch)
 
     def get(self):
         result = []
-        if self.checkbox_1.get() == 1:
-            result.append("Yolo [1] selected")
-        if self.checkbox_2.get() == 1:
-            result.append("Yolo [2] selected")
-        if self.checkbox_3.get() == 1:
-            result.append("Yolo [3] selected")
+        for switch in self.switches:
+            result.append(switch.get())
         return result
 
 
@@ -33,7 +28,7 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.checkbox_frame = CheckboxFrame(self)
+        self.checkbox_frame = TrueFalseFrame(self, values=["Option 1", "Option 2", "Option 3", "Option 4"])
         self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nswe")
 
         self.btn = customtkinter.CTkButton(self, text="Click me!", command=self.btn_callback)
